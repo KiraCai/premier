36333808dcd8ebe63a4d73a278c951a732e2883c
KiraCai <kiraswav@gmail.com>
2023-05-25 16:24:35 +0200

    Task3.2.2

--- src/main/java/task322/Main.java
+++ src/main/java/task322/Main.java
@@ -4,7 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
         byte varbyte = 56;
+        short varshort = 31000;
+        int varint = 65399990;
+        long varlong = 908435098530948L;
+        char varchar = 'r';
+        float varfloat = 89.390485092f;
+        double vardouble = 54.03943;
+        boolean varboolean = true;
 
         System.out.println(varbyte);
+        System.out.println(varshort);
+        System.out.println(varint);
+        System.out.println(varlong);
+        System.out.println(varchar);
+        System.out.println(varfloat);
+        System.out.println(vardouble);
+        System.out.println(varboolean);
     }
 }

c3bbf06b30ec7487081d812a58874c789187ab60
KiraCai <kiraswav@gmail.com>
2023-05-26 12:51:25 +0200

    Task3.3.2

---
+++ src/main/java/task342/Task342.java
@@ -0,0 +1,20 @@
+package src.main.java.task342;
+
+/**
+ * Создайте метод public static void printHelloWorld(), который будет выводить в консоль строку “Hello world!”.
+ *
+ * Требования:
+ * 1. Метод должен иметь модификатор static
+ * 2. Метод не должен принимать параметры
+ * 3. Метод должен выводить в консоль строку “Hello world!”
+ * 4. Метод не должен ничего возвращать
+ */
+public class Task342 {
+    public static void main(String[] args) {
+        printHelloWorld();
+    }
+    public static void printHelloWorld() {
+        String e = "Hello world!";
+        System.out.println(e);
+    }
+}

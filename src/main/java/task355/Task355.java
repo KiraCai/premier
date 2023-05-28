875e0034bb5f975e65ea88e7eddd5c5f8d77eb39
KiraCai <kiraswav@gmail.com>
2023-05-29 00:19:04 +0200

    Task3.5.5

---
+++ src/main/java/task355/Task355.java
@@ -0,0 +1,26 @@
+package src.main.java.task355;
+
+/**
+ * Создайте метод static boolean determineAdult(int age), который будет принимать возраст человека и возвращать логическое значение, равное true если он 18 и больше и false если он меньше 18, если переданное число меньше 0, метод должен вывести в консоль “Ошибка! Введите правильный возраст.” и вернуть false.
+ *
+ * Требования:
+ * 1. Метод должен иметь модификаторы public static
+ * 2. Метод должен принимать в качестве параметра целое число
+ * 3. Метод должен возвращать логическое значение
+ */
+public class Task355 {
+    public static void main(String[] args){
+        System.out.println(determineAdult(6));
+    }
+    public static boolean determineAdult(int age) {
+        boolean bool = false;
+        if (age>= 18){
+            bool = true;
+        }
+        else if (age <0) {
+            System.out.println("Ошибка! Введите правильный возраст.");
+        }
+        return bool;
+    }
+
+}

9ce6adf9ac3ac24234277bb46bceeb76802e824f
leonid <pcjmeil@gmail.com>
2023-05-29 22:08:47 +0300

    CR 29.05.23

--- src/main/java/prepay/task362/Task362.java
+++ src/main/java/prepay/task362/Task362.java
@@ -19,6 +19,7 @@ public class Task362 {
         System.out.println(determineRainfall(8,12));
     }
     public static long determineRainfall(double area, double rainfallAmount) {
+        //NIT большие числа писать с разделителем 1_000_000
         return (long) (rainfallAmount * 2000000 * area);
     }
 }
--- src/main/java/prepay/task381/Task381.java
+++ src/main/java/prepay/task381/Task381.java
@@ -17,8 +17,8 @@ import java.util.Scanner;
  * 3. Второй метод должен использовать первый
  * 4. Методы не должны ничего выводить в консоль
  */
-import java.util.Scanner;
 public class Task381 {
+    //NIT перепутала - возвращать наименьший, а не наоборот
     public static void main(String[] args){
         int x = 5;
         int y = 6;
@@ -28,6 +28,10 @@ public class Task381 {
     public static int getMinFromTwo(int x, int y){
         return Math.max(x, y);
     }
+
+    //NIT
+    // Во первых, можно было бы и max2 вычислить с помощью метода getMinFromTwo
+    // Во вторых, можно тогда и не объявлять max2 раз у тебя нет результата сравнения для другой пары
     public static int getMinFromFour(int x, int y, int m, int n){
         int max2 = Math.max(m, n);
         return Math.max(getMinFromTwo(x, y), max2);

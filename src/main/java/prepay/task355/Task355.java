ba21eee239cafdf4826087370bfb44124b0ae838
leonid <pcjmeil@gmail.com>
2023-05-29 09:36:27 +0300

    CR 29.05.23

--- src/main/java/prepay/task355/Task355.java
+++ src/main/java/prepay/task355/Task355.java
@@ -1,7 +1,10 @@
 package src.main.java.prepay.task355;
 
+// NIT По возможности текст не должен выходить за границы
 /**
- * Создайте метод static boolean determineAdult(int age), который будет принимать возраст человека и возвращать логическое значение, равное true если он 18 и больше и false если он меньше 18, если переданное число меньше 0, метод должен вывести в консоль “Ошибка! Введите правильный возраст.” и вернуть false.
+ * Создайте метод static boolean determineAdult(int age), который будет принимать возраст человека и возвращать логическое значение,
+ * равное true если он 18 и больше и false если он меньше 18,
+ * если переданное число меньше 0, метод должен вывести в консоль “Ошибка! Введите правильный возраст.” и вернуть false.
  *
  * Требования:
  * 1. Метод должен иметь модификаторы public static
@@ -12,12 +15,15 @@ public class Task355 {
     public static void main(String[] args){
         System.out.println(determineAdult(6));
     }
+
+    //FIXME
+    // Имя переменной должно отражать смысл значения. Проинициалиировать сразу условием (bool b = "condition").
+    // Определить условие в конструкции if-else так, чтобы не пришлось использовать 'else if'
     public static boolean determineAdult(int age) {
         boolean bool = false;
         if (age>= 18){
             bool = true;
-        }
-        else if (age <0) {
+        } else if (age <0) {
             System.out.println("Ошибка! Введите правильный возраст.");
         }
         return bool;

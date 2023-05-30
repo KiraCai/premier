6e96d0e159669639fe2a2d3e71529af9e4742c2c
KiraCai <kiraswav@gmail.com>
2023-05-30 18:59:02 +0200

    3.10.3

--- src/main/java/prepay/task3103/Human.java
+++ src/main/java/prepay/task3103/Human.java
@@ -16,20 +16,14 @@ package src.main.java.prepay.task3103;
  * 6. У объекта класса Human должен быть вызван метод sayHello()
  * 7. Класс Human должен быть статическим, метод main находится вне этого класса
  */
-public class Main {
-    static Task3103 human = new Task3103();
+public class Human {
+    static String name = "Денис";
+    static int age = 5;
     static public void main(String[] args) {
-        human.age;
-        human.name;
-        System.out.println(human);
+        Human hum = new Human();
+        hum.sayHello(name, age);
     }
-}
-public class Task3103 {
-    String name = "Денис";
-    int age = 8;
-    Task3103 human;
-    @Override
-    public String toString() {
-        return super.toString("Привет, меня зовут " + name + ", мне" + age + "лет.");
+    static public void sayHello(String namePers, int agePers){
+        System.out.println("Привет, меня зовут " + namePers + ", мне " + agePers + " лет.");
     }
 }

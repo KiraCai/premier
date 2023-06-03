0f65b321e92604060d05c7c0701ef643553474a6
KiraCai <kiraswav@gmail.com>
2023-06-03 16:29:37 +0200

    game2

--- src/main/java/prepay/task400/Game2.java
+++ src/main/java/prepay/task400/Game2.java
@@ -1,5 +1,8 @@
 package src.main.java.prepay.task400;
 
+import java.util.Scanner;
+import java.util.Random;
+
 /**
  * Реализовать игру, предлагающая игроку угадать загаданное ей число.
  * При запуске игры игроком вводится диапозон, в котором будет лежать загадываемое число.
@@ -23,5 +26,35 @@ package src.main.java.prepay.task400;
  */
 public class Game2 {
     public static void main(String[] args) {
+        int count = 1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Старт игры");
+        System.out.println("Введите нижний предел");
+        int lowLim = scanner.nextInt();
+        System.out.println("Введите верхний предел");
+        int highLim = scanner.nextInt();
+        System.out.println("Число загадано");
+        //две строки ниже из интернета, но я прочитала обоснование и поняла
+        highLim -= lowLim;
+        int i = ((int) (Math.random() * ++highLim)) + lowLim;
+        System.out.println("Введите число");
+        int num = scanner.nextInt();
+        if (num != i) {
+            while (num != i) {
+                count ++;
+                if (num<i) {
+                    System.out.println("Неверно. Загаданное число больше. Введите новое число");
+                    num = scanner.nextInt();
+                }
+                else {
+                    System.out.println("Неверно. Загаданное число меньше. Введите новое число");
+                    num = scanner.nextInt();
+                }
+            }
+            System.out.println("Верно. Загаданное число определено за " + count + " попытки");
+        }
+        else {
+            System.out.println("Верно. Загаданное число определено за 1 попыткy");
+        }
     }
 }

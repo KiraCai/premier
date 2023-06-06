ee745b93ef0545bd043e4ab70acba685cf39dcc6
KiraCai <kiraswav@gmail.com>
2023-06-06 20:55:15 +0200

    132

---
+++ src/main/java/Core/task132/Task132.java
@@ -0,0 +1,21 @@
+package src.main.java.Core.task132;
+
+import java.util.Arrays;
+
+/**
+ * Реализуйте метод, который возвращает букву, стоящую в таблице UNICODE после символа "\" (обратный слэш) на расстоянии a.
+ * В качестве примера написано заведомо неправильное выражение. Исправьте его.
+ *
+ * Требования:
+ * 1. Сигнатура метода должна быть: charExpression(int a)
+ */
+public class Task132 {
+    public static void main(String[] args){
+        int integer = 61;
+        System.out.println(charExpression(integer));
+    }
+    public static String charExpression(int integerInput) {
+        char[] character = Character.toChars(integerInput);
+        return Arrays.toString(character);
+    }
+}

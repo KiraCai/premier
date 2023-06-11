6b09ef5d01deb0f30cc8f9a3552038e9605fc630
KiraCai <kiraswav@gmail.com>
2023-06-11 21:15:32 +0200

    испр 1210

--- src/main/java/Core/task1210/Task1210.java
+++ src/main/java/Core/task1210/Task1210.java
@@ -14,20 +14,14 @@ import java.math.BigDecimal;
  */
 public class Task1210 {
     public static void main(String[] args){
-        BigDecimal aNumber = BigDecimal.valueOf(5.909870989);
-        BigDecimal bNumber = BigDecimal.valueOf(8.32458);
-        BigDecimal cNumber = BigDecimal.valueOf(3.078886);
-        int zero = 0;
+        double aNumber = 5.909870989;
+        double bNumber = 8.32458;
+        double cNumber = 3.078886;
+        double tol = 0.0001;
         System.out.println("правда ли, что a + b = c ?");
-        if (doubleExpression(aNumber, bNumber, cNumber) == zero) {
-            System.out.println("True");
-        } else {System.out.println("False");
-        }
+        System.out.println(doubleExpression(aNumber, bNumber, cNumber, tol));
     }
-    public static int doubleExpression(BigDecimal a, BigDecimal b, BigDecimal c) {
-        BigDecimal summaAB = a.add(b);
-        BigDecimal deltaStatic = BigDecimal.valueOf(0.0001);
-        BigDecimal differenceBetween_A_B_And_C = summaAB.subtract(c);
-        return differenceBetween_A_B_And_C.compareTo(deltaStatic);
+    public static boolean doubleExpression(double a, double b, double c, double toleranse) {
+        return a + b - c == toleranse;
     }
 }

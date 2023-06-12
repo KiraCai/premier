f7bb37e6890fa266a022f3e99f40bfc7d88a6c82
KiraCai <kiraswav@gmail.com>
2023-06-12 20:03:37 +0200

    1411

---
+++ src/main/java/Core/task1411/Task1411.java
@@ -0,0 +1,39 @@
+package src.main.java.Core.task1411;
+
+/**
+ * Цель: Работа с оператором if/else
+ *
+ * Что нужно знать:
+ *
+ * 1. Оператор if
+ *
+ * Задание:
+ * Реализовать метод, который будет принимать строку с названием дня недели, и отвечать, является ли этот день  выходным.
+ * Список дней недели:
+ * "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday".
+ * Если строка не соответствует ни одному дню недели отправлять false.
+ *
+ * Пример ввода: Monday
+ * Пример вывода: false
+ * Пример ввода: Saturday
+ * Пример вывода: true
+ * Требования:
+ * метод должен быть public static
+ * сигнатура метода isWeekend(String weekDay)
+ * метод возвращает boolean
+ * решить с использование оператора if
+ */
+public class Task1411 {
+    public static void main(String[] args) {
+        String day = "Tuesday";
+        System.out.println(isWeekend(day));
+    }
+    public static boolean isWeekend(String weekday) {
+        boolean boo = false;
+        if (weekday.equals("Saturday") || weekday.equals("Sunday")){
+            boo = true;
+        }
+        else {boo = false;}
+        return boo;
+    }
+}

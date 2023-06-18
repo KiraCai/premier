32ad4252f8a3aeadcb1cf76b08da97befef6271d
KiraCai <kiraswav@gmail.com>
2023-06-18 17:00:49 +0200

    157

--- src/main/java/Core/task157/Task157.java
+++ src/main/java/Core/task157/Task157.java
@@ -1,5 +1,11 @@
 package src.main.java.Core.task157;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.List;
+
 /**
  * Цель: Слить два произвольных массива в один отсортированный
  * Что нужно знать:
@@ -24,9 +30,35 @@ public class Task157 {
         mergeAndSort(num, mi);
     }
     public static void mergeAndSort(int[] firstArray, int[] secondArray) {
+        int aLen = firstArray.length;
+        int bLen = secondArray.length;
+        int cLen = aLen + bLen;
+        int[] naitre = new int[cLen];
+        int[] n = new int[cLen];
         for (int i = 0; i < firstArray.length; i++) {
-            int[] n = int[firstArray.length + secondArray.length];
-            int[i] =
+            n[i] = firstArray[i];
+        }
+        for (int i = 0; i < secondArray.length; i++) {
+            n[i+aLen] = secondArray[i];
+        }
+        int constantaLen = n.length;
+        for (int i = 0; i < constantaLen; i++){
+            List<Integer> list = Arrays.stream(n).boxed().collect(Collectors.toList());
+            List<Integer> copy = list.stream().collect(Collectors.toList());
+            Integer mi = Collections.min(list);
+            naitre[i] = mi;
+            list.removeIf(j -> (j == mi));
+            int count = list.size();
+            int countCopy = copy.size();
+            int delta = countCopy - count;
+            if (delta >1){
+                delta = delta - 1;
+                for (int g = 0; g<delta; g++){
+                    list.add(mi);
+                }
+            }
+            n = list.stream().mapToInt(Integer::intValue).toArray();
+            System.out.println(naitre[i]);
         }
     }
 }

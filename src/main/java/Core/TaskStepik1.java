90f59df7fe90b4670f35e2ba33d10d13fa3714c7
KiraCai <kiraswav@gmail.com>
2023-07-05 16:17:26 +0200

    задача из степика

---
+++ src/main/java/Core/TaskStepik1.java
@@ -0,0 +1,17 @@
+package src.main.java.Core;
+
+public class TaskStepik1 {
+    public static void main(String[] args){
+        String text1 = "Madam, I'm Adam!";
+        System.out.println(isPalindrome(text1));
+    }
+    //использование регулярных выражений
+    public static boolean isPalindrome(String text) {
+        String regex = "[^a-zA-Z0-9]";
+        String text1 = text.replaceAll(regex, "");
+        String text2 = text1.toLowerCase();
+        StringBuffer buffer = new StringBuffer(text2);
+        //String reverseText = buffer.reverse().toString();
+        return text2.equals(buffer.reverse().toString());
+    }
+}

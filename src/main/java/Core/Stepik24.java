30cbcd5cd06f1f63a60cf891fa9429a9eb076929
KiraCai <kiraswav@gmail.com>
2023-12-20 19:17:33 +0100

    ok

--- src/main/java/Core/Stepik24.java
+++ src/main/java/Core/Stepik24.java
@@ -90,6 +90,7 @@ public class Stepik24 {
                             k++;
                             j++;
                             continue;
+
                         }
                     }
                 } else {

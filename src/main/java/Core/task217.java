ec73ee55b88a0d16e2087a8e5218279105fc9764
KiraCai <kiraswav@gmail.com>
2024-01-28 00:43:26 +0100

    новое задание2

---
+++ src/main/java/Core/task217.java
@@ -0,0 +1,37 @@
+package src.main.java.Core;
+
+/*
+Работа с Enum
+Цель: научиться создавать перечисления (Enum).
+Что нужно знать:
+1. Enum
+Задание:
+Реализовать Enum Day, который будет отвечать за дни недели. Этот Enum должен содержать следующие элементы:
+MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+Также в этом Enum должны быть релизованы два метода:
+1. public boolean isWeekend() — метод, отвечающий на вопрос, является ли конкретный элемент выходным днем.
+ Выходными днями являются только суббота и воскресенье.
+2. public String getRusName() — метод, возвращающий русское название дня недели.
+Требования:
+Enum должен иметь название Day.
+Enum должен содеражить элементы на каждый день недели.
+Сигнатура метода isWeekend().
+метод isWeekend возвращает boolean.
+Сигнатура метода getRusName().
+Метод getRusName возвращает String.
+ */
+public class task217 {
+    public static void main(String[] args){
+
+    }
+    enum Day{
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,
+        SUNDAY
+
+    }
+}

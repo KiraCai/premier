213aae835d53cf4d2b14711dbba9a73e41a0ea0e
KiraCai <kiraswav@gmail.com>
2024-01-27 21:13:34 +0100

    переделала3

--- src/main/java/Core/Leonid11.java
+++ src/main/java/Core/Leonid11.java
@@ -11,28 +11,23 @@ import java.util.Arrays;
  */
 public class Leonid11 {
     public static void main(String[] args){
-        int[] massiv = new int[] {2, 4, 5, 1, 8};
+        int[] array = new int[] {2, 4, 5, 1, 8};
         int numX = 5;
-        int[] massiv1 = new int[] {2, 4, 5, 9, 8};
+        int[] array1 = new int[] {2, 4, 5, 9, 8};
         int numX1 = 5;
-        int[] massiv2 = new int[] {2, 4, 5, 9, 8};
-        int numX2 = 7;
-        System.out.println(Arrays.toString(ArraySum(massiv, numX)));
-        System.out.println(Arrays.toString(ArraySum(massiv1, numX1)));
-        System.out.println(Arrays.toString(ArraySum(massiv2, numX2)));
+        System.out.println(Arrays.toString(ArraySum(array, numX)));
+        System.out.println(Arrays.toString(ArraySum(array1, numX1)));
     }
     public static int[] ArraySum(int[] a, int b){
         int[] arrSum = new int[2];
-        //создадим новый массив вычев из пяти все значения и сравним с массивом какое совпадет то и берём
-        int count = 0;
-        for (int j = 0; j < a.length; j++) {
-            for (int f = 0; f < a.length; f++) {
-                if (a[j] + a[f] == b) {
-                    return new int[]{j, f};
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                if (a[i] + a[j] == b) {
+                    return new int[]{i, j};
                 }
             }
         }
-        if (arrSum[1] == 0) {
+        if (arrSum[1] == 0) {  //да, потому что если нет, он выведет массив с двумя нулями а не с одним пустой
             return new int[0];
         }
         return arrSum;

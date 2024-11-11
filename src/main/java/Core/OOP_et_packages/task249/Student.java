5bedfb25d4e91d8c2ed310ee469d7cdab9ea58e7
Kira Korelskaia <kiraswav@gmail.com>
2024-11-11 20:26:29 +0100

    внесены изменения

--- src/main/java/Core/OOP_et_packages/task249/Student.java
+++ src/main/java/Core/OOP_et_packages/task249/Student.java
@@ -19,12 +19,15 @@ package src.main.java.Core.OOP_et_packages.task249;
  */
 public class Student {
     protected String studying;
+
     protected Student(String work) {
         this.studying = work;
     }
+
     public Student() {
         this.studying = "Прохожу тестовое задание.";
     }
+
     public void study() {
         System.out.println("Я очень занят. " + studying);
     }
--- src/main/java/Core/OOP_et_packages/task249/Task249.java
+++ src/main/java/Core/OOP_et_packages/task249/Task249.java
@@ -1,15 +1,8 @@
 package src.main.java.Core.OOP_et_packages.task249;
 
-public class JavaStudent extends Student {
-
-    public JavaStudent() {
-        super();
-        super.studying = "Прохожу курс по Java";
-    }
-
+public class Task249 extends JavaStudent {
     public static void main(String[] args) {
         JavaStudent studentJ = new JavaStudent();
         studentJ.study();
     }
-
 }
